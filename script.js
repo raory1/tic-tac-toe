93a596db9ea540a9fedd729958e614d3c799fdce
Isabella <isa021andrade@gmail.com>
2025-06-30 21:52:38 -0300

    feat: create an IIFE gameController to play rounds

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function Gameboard() {
 
     const placeMarker = (row, col, marker) => {
         if (board[row][col] === 0) {
+            console.log("marcador adicionado");
             board[row][col] = marker;
             return true;
         }
@@ -27,8 +28,20 @@ function createPlayer(name, marker) {
     return { name, marker };
 }
 
+const gameController = (function () {
+    const players = [createPlayer("Isa", "X"), createPlayer("Dani", "O")];
+    let currentPlayer = players[0];
+    const playRound = (row, column) => {
+        board.placeMarker(1, 2, currentPlayer.marker);
+    };
+
+    return { playRound };
+})();
+
 const board = Gameboard();
 
 board.placeMarker(0, 1, 2);
 console.log(board.getBoard());
 
+gameController.playRound(1,2);
+console.log(board.getBoard());
