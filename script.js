3a303b587c0732d084dab917d82a8babcd6a91ab
Isabella <isa021andrade@gmail.com>
2025-07-01 15:14:31 -0300

    feat: implement winner check logic after each move

--- script.js
+++ script.js
@@ -21,7 +21,42 @@ function Gameboard() {
         console.log("a casa está ocupada");
         return false;
     };
-    return { getBoard, placeMarker };
+
+    const checkWinner = () => {
+        if (
+            // check rows
+            (board[0][0] !== 0 &&
+                board[0][0] === board[0][1] &&
+                board[0][0] === board[0][2]) ||
+            (board[1][0] !== 0 &&
+                board[1][0] === board[1][1] &&
+                board[1][0] === board[1][2]) ||
+            (board[2][0] !== 0 &&
+                board[2][0] === board[2][1] &&
+                board[2][0] === board[2][2]) ||
+            // check columns
+            (board[0][0] !== 0 &&
+                board[0][0] === board[1][0] &&
+                board[0][0] === board[2][0]) ||
+            (board[0][1] !== 0 &&
+                board[0][1] === board[1][1] &&
+                board[0][1] === board[2][1]) ||
+            (board[0][2] !== 0 &&
+                board[0][2] === board[1][2] &&
+                board[0][2] === board[2][2]) ||
+            // check diagonals
+            (board[0][0] !== 0 &&
+                board[0][0] === board[1][1] &&
+                board[0][0] === board[2][2]) ||
+            (board[2][0] !== 0 &&
+                board[2][0] === board[1][1] &&
+                board[2][0] === board[0][2])
+        ) {
+            return 1;
+        }
+        return 0;
+    };
+    return { getBoard, placeMarker, checkWinner };
 }
 
 function createPlayer(name, marker) {
@@ -34,12 +69,18 @@ const gameController = (function () {
 
     const playRound = (row, column) => {
         board.placeMarker(row, column, currentPlayer.marker);
-        switchPlayer();
+        const hasWinner = board.checkWinner();
+        if (!hasWinner) {
+            switchPlayer();
+        } else {
+            console.log(`${currentPlayer.name} venceu!`);
+        }
     };
 
     const switchPlayer = () => {
         currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
     };
+
     return { playRound };
 })();
 
@@ -51,4 +92,9 @@ gameController.playRound(1, 2);
 gameController.playRound(0, 2);
 gameController.playRound(1, 0);
 gameController.playRound(0, 0);
+gameController.playRound(0, 1);
+gameController.playRound(1, 1);
+gameController.playRound(2, 1);
+gameController.playRound(2, 2);
+
 console.log(board.getBoard());
