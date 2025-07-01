e856a233ca40466f0c7f82d5321f2fe7b6806d72
Isabella <isa021andrade@gmail.com>
2025-06-30 21:28:40 -0300

    feat: create a player object

---
+++ script.js
@@ -0,0 +1,19 @@
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(0);
+        }
+    }
+    console.log(board);
+}
+
+function createPlayer(name, marker){
+    return {name, marker}
+}
+
+Gameboard();
